f3d9b5fe89e36e2199d84e7c4e3da4dd7b1eb71d
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-04 07:26:31 +1100

    Readded chat_downloader
    Added with fallback processes

--- downloadVid.sh
+++ downloadVid.sh
@@ -20,7 +20,10 @@ python /app/discord-web.py "$1" "recording"
 output="$tempdir/$partialoutput"
 # Begin downloading live chat for the stream once it begins in parallel
 {
-         yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" || echo "Error downloading chat for $1"
+        chat_downloader --cookies /app/cookies.txt -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" || \
+chat_downloader -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1"
+yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" \
+|| echo "Error downloading chat for $1"
         # If the stream is privated at the end (or some other event cuts your access to the stream),
         # the chat file will not be closed correctly, and the .part extension can be removed
         if [[ -f "$output.live_chat.json.part" ]]
