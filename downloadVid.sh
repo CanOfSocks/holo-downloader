1055b853a218e42e018e9c4687f7d379c27966bf
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-03 14:55:29 +1100

    Update downloadVid.sh

--- downloadVid.sh
+++ downloadVid.sh
@@ -20,7 +20,7 @@ python /app/discord-web.py "$1" "recording"
 output="$tempdir/$partialoutput"
 # Begin downloading live chat for the stream once it begins in parallel
 {
-        yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1"
+        yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" || echo "Error downloading chat for $1"
         # If the stream is privated at the end (or some other event cuts your access to the stream),
         # the chat file will not be closed correctly, and the .part extension can be removed
         if [[ -f "$output.live_chat.json.part" ]]
@@ -28,12 +28,12 @@ output="$tempdir/$partialoutput"
                 mv "$output.live_chat.json.part" "$output.live_chat.json"
         fi
         # Compress the live_chat.json file, this can save 80-90% of the space usually taken by these files
-        zip -9 -m "$(printf '%s\n' '$output.live_chat.json' | sed 's/\.json//').zip" "$output.live_chat.json"
+        zip -9 -m "$(printf '%s\n' '$output.live_chat.json' | sed 's/\.json//').zip" "$output.live_chat.json"  || echo "Error compressing chat for $1"
 } &
 # Download the metadata (.info.json) and thumbnail in parallel
 {
         yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --live-from-start --write-info-json --write-thumbnail --convert-thumbnails png --write-description --skip-download -o "$output" "$1" \
-&& perl -pi -e "s/((?:[0-9]{1,3}\.){3}[0-9]{1,3})|((?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4})/0\.0\.0\.0/g" "$output.info.json"
+&& perl -pi -e "s/((?:[0-9]{1,3}\.){3}[0-9]{1,3})|((?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4})/0\.0\.0\.0/g" "$output.info.json" || echo "Error gathering video data (e.g. info.json) chat for $1"
 } &
 # Download the video/audio (from the start), preferring VP9 codec
 ytarchive --cookies /app/cookies.txt -t --vp9 --retry-stream 15 --threads 4 --no-frag-files --output "$output" "https://www.youtube.com/watch?v=$1" "best" \
