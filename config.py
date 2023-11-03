855d29a64e926813efe6b9749dc8f271c035a32e
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-03 13:34:38 +1100

    Update config.py

--- config.py
+++ config.py
@@ -2,21 +2,9 @@
 channel_ids_to_match = {
     "Gawr Gura Ch. hololive-EN": "UCoSrY_IQQVpmIRZ9Xf-y93g",
     "Watson Amelia Ch. hololive-EN": "UCyl1z3jo3XHR1riLFKG5UAg",
-    "Mori Calliope Ch. hololive-EN": "UCL_qhgtOy0dy1Agp8vkySQg",
-    "Takanashi Kiara Ch. hololive-EN": "UCHsx4Hqa-1ORjQTh9TYDhww",
-    "Nanashi Mumei Ch. hololive-EN": "UC3n5uGu18FoCy23ggWWp8tA",
-    "Hakos Baelz Ch. hololive-EN": "UCgmPnx-EEeOrZSg5Tiw7ZRQ",
-    "Ceres Fauna Ch. hololive-EN": "UCO_aKKYxn4tvrqPjcTzZ6EQ",
-    "Ouro Kronii Ch. hololive-EN": "UCmbs8T6MWqUHP1tIQvSgKrg",
-    "IRyS Ch. hololive-EN": "UC8rcEBzJSleTkf_-agPM20g",
-    "Ninomae Ina'nis Ch. hololive-EN": "UCMwGHR0BTZuLsmjY_NT5Pwg",
-    "Watson Amelia Ch. hololive-EN": "UCyl1z3jo3XHR1riLFKG5UAg",
-    "Shiori Novella Ch. hololive-EN": "UCgnfPPb9JI3e9A4cXHnWbyg",
-    "Koseki Bijou Ch. hololive-EN": "UC9p_lqQ0FEDz327Vgf5JwqA",
-    "Nerissa Ravencroft Ch. hololive-EN": "UC_sFNM0z0MWm9A6WlKPuMMg",
-    "FUWAMOCO Ch. hololive-EN": "UCt9H_RpQzhxzlyBxFqrdHqA"
+    "Mori Calliope Ch. hololive-EN": "UCL_qhgtOy0dy1Agp8vkySQg"
     
     # Add more channels as needed
 }
 
-webhook_url = "https://discord.com/api/webhooks/1005627351402557532/j5Y_HkD5OlOnRtCSuwnOLEc9drkqhKI7S5PZcAg-7N1xCmqeC5RepkTIE1qFSuiXyIXa"
+webhook_url = "https://discord.com/api/webhooks/xxxxxxxxxxxxx/yyyyyyyyyyyyyyyyyyyyyyyyy"
