a084d770bb99d6fcdba67f2ad575ff726243e693
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-06 13:08:41 +1100

    Changed json handler to class
    Changed json handler to class for better expansion options in the future.
    Removed importing of json library as it appears to not be needed (according to it working in vs code?)

--- getJson.py
+++ getJson.py
@@ -1,10 +1,31 @@
 import requests
-import json
+#import json
 from datetime import datetime,timezone
-from config import channel_ids_to_match
+from config import channel_ids_to_match,look_ahead
 
 url = "https://holo.dev/api/v1/lives/open"
 
+class json_object:
+    def __init__(self,live):
+        self.id=live.get('id')
+        self.title=live.get('title')
+        date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+        self.start_at = datetime.strptime(live.get('start_at'), date_format)
+        self.created_at = datetime.strptime(live.get('created_at'), date_format)
+        self.json_channel_id = live.get('channel_id')
+        self.thumbnail_url = live.get('cover')
+        
+        self.video_id = live.get('room')
+        self.platform = live.get('platform')
+        self.channel_id = live.get('channel')
+        
+        self.duration = live.get('duration') if live.get('duration') is not None else -1
+    def time_until_start(self):
+        current_datetime = datetime.now(timezone.utc)
+        time_difference = self.start_at - current_datetime.replace(tzinfo=None)
+        return time_difference.total_seconds()
+        
+        
 
 
 # List to store the matching live streams
@@ -18,23 +39,25 @@ if response.status_code == 200:
     # Parse the JSON data from the response
     data = response.json()
 
+    videos = []
     # Check if the "lives" key exists in the JSON data
     if 'lives' in data:
+        # Add all videos to class list
         for live in data['lives']:
-            channel_id = live.get('channel')
+            videos.append(json_object(live))
+            
+        #Run for each class object made
+        for live in videos:            
             # Check if the "channel_id" is in the dictionary
-            #print(channel_id)
-            if channel_id in channel_ids_to_match.values():
+            #print(live.channel_id)
+            if live.channel_id in channel_ids_to_match.values():
                 # Find the key (descriptive name) corresponding to the matched channel_id
-                channel_name = [key for key, value in channel_ids_to_match.items() if value == channel_id][0]
-                if(live.get('platform') == "youtube"):
-                    date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-                    parsed_date = datetime.strptime(live.get('start_at'), date_format)
-                    current_datetime = datetime.now(timezone.utc)
-                    time_difference = parsed_date - current_datetime 
+                #channel_name = [key for key, value in channel_ids_to_match.items() if value == channel_id][0]
+                
+                if(live.platform == "youtube"):
                     #print(time_difference)
-                    if(time_difference.total_seconds() <= 48 * 3600):
-                        matching_streams.append(live.get('room'))
+                    if(live.time_until_start() <= look_ahead * 3600):
+                        matching_streams.append(live.video_id)
 
 # Print the list of matching streams as a JSON representation
 #matching_streams_json = json.dumps(matching_streams)
