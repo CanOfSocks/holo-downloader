e5bc8d8352283ff1fa000f1f3da2e4c703b479e3
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-06 20:15:27 +1100

    Trying "quiet" mode for chat-downloader

--- downloadVid.sh
+++ downloadVid.sh
@@ -25,8 +25,8 @@ python /app/discord-web.py "$1" "recording"
 output="$tempdir/$partialoutput"
 # Begin downloading live chat for the stream once it begins in parallel
 {
-        chat_downloader --cookies /app/cookies.txt --logging error -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" > /dev/null || \
-chat_downloader --logging error -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1"  > /dev/null || \
+        chat_downloader -q --cookies /app/cookies.txt --logging error -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" || \
+chat_downloader -q --logging error -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" || \
 yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" \
 || echo "Error downloading chat for $1"
         # If the stream is privated at the end (or some other event cuts your access to the stream),
