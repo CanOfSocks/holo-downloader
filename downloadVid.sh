034947ecbbd662bec0623357e1bef54b81d2a384
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-20 12:56:12 +1100

    Change python methods to arrays

--- downloadVid.sh
+++ downloadVid.sh
@@ -21,9 +21,9 @@ donedir="/app/Done"
 
 python /app/discord-web.py "$1" "waiting"
 
-ytdlpOptions=$(python /app/getConfig.py "yt-dlp_options")
+ytdlpOptions=($(python /app/getConfig.py "yt-dlp_options"))
 # Waits for the stream to begin and creates a variable with the stream title and ID
-partialoutput=$(yt-dlp --wait-for-video 1-300 -R 25 --skip-download $ytdlpOptions --print "%(filename)s" --no-warnings "$1" 2>&1 | tail -n 1) || (python /app/discord-web.py "$1" && exit)
+partialoutput=$(yt-dlp --wait-for-video 1-300 -R 25 --skip-download "${ytdlpOptions[@]}" --print "%(filename)s" --no-warnings "$1" 2>&1 | tail -n 1) || (python /app/discord-web.py "$1" && exit)
 echo "$partialoutput"
 python /app/discord-web.py "$1" "recording"
 output="$tempdir/$partialoutput"
@@ -32,8 +32,8 @@ getChat=$(python /app/getConfig.py "get_chat")
 if [[ "$getChat" == "True" ]]; then
         # Begin downloading live chat for the stream once it begins in parallel
         {
-                cookies=$(python /app/getConfig.py "cookies")
-                chat_downloader $cookies --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
+                cookies=($(python /app/getConfig.py "cookies"))
+                chat_downloader "${cookies[@]}" --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
 chat_downloader --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
 yt-dlp $cookies --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" \
 || echo "Error downloading chat for $1"
@@ -48,20 +48,20 @@ yt-dlp $cookies --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-f
         } &
         chat_pid=$!
 fi
-infoOptions=$(python /app/getConfig.py "info_options")
+infoOptions=($(python /app/getConfig.py "info_options"))
 if ! [ -z "${infoOptions}" ]; then
         # Download the metadata (.info.json) and thumbnail in parallel
         {
-                yt-dlp --wait-for-video 1-15 -R 25 --live-from-start $infoOptions --skip-download -o "$output" "$1" \
+                yt-dlp --wait-for-video 1-15 -R 25 --live-from-start "${infoOptions[@]}" --skip-download -o "$output" "$1" \
 && (test -f "$output.info.json" && perl -pi -e "s/((?:[0-9]{1,3}\.){3}[0-9]{1,3})|((?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4})/0\.0\.0\.0/g" "$output.info.json") || echo "Error gathering video data (e.g. info.json) chat for $1"
         } &
         info_pid=$!
 fi
 
 #Download the video/audio (from the start), preferring VP9 codec
-ytarchiveOptions=$(python /app/getConfig.py "ytarchive_options")
+ytarchiveOptions=($(python /app/getConfig.py "ytarchive_options"))
 quality=$(python /app/getConfig.py "quality")
-ytarchive $ytarchiveOptions --error --output "$output" "https://www.youtube.com/watch?v=$1" "$quality" \
+ytarchive "${ytarchiveOptions[@]}" --error --output "$output" "https://www.youtube.com/watch?v=$1" "$quality" \
 && success="True" || (python /app/discord-web.py "$1" "error" ; kill -2 $chat_pid $info_pid)
 
 # Wait for all above processes to complete
