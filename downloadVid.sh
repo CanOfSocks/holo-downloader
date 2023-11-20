e02bccdebc34ff779f41239059c9fb333dc8b823
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-20 11:23:04 +1100

    Update downloadVid.sh to use config gatherer

--- downloadVid.sh
+++ downloadVid.sh
@@ -14,55 +14,63 @@ trap "kill 0" EXIT
 
 
 
-success="false"
+success="False"
 
 tempdir="/app/temp"
 donedir="/app/Done"
 
 python /app/discord-web.py "$1" "waiting"
+
+ytdlpOptions=$(python /app/getConfig.py "yt-dlp_options")
 # Waits for the stream to begin and creates a variable with the stream title and ID
-partialoutput=$(yt-dlp --cookies /app/cookies.txt --wait-for-video 1-300 -R 25 --skip-download -o "%(channel)s/[%(upload_date)s] %(fulltitle)s - %(channel)s (%(id)s)/[%(upload_date)s] %(fulltitle)s - %(channel)s (%(id)s)" --print "%(filename)s" --no-warnings "$1" 2>&1 | tail -n 1) || (python /app/discord-web.py "$1" && exit)
+partialoutput=$(yt-dlp --wait-for-video 1-300 -R 25 --skip-download $ytdlpOptions --print "%(filename)s" --no-warnings "$1" 2>&1 | tail -n 1) || (python /app/discord-web.py "$1" && exit)
 
 python /app/discord-web.py "$1" "recording"
 output="$tempdir/$partialoutput"
-# Begin downloading live chat for the stream once it begins in parallel
-{
-        chat_downloader --cookies /app/cookies.txt --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
+
+getChat=$(python /app/getConfig.py "get_chat")
+if [[ "$getChat" == "True" ]]
+        # Begin downloading live chat for the stream once it begins in parallel
+        {
+                cookies=$(python /app/getConfig.py "cookies")
+                chat_downloader $cookies --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
 chat_downloader --logging critical -o "$output.live_chat.json" "https://www.youtube.com/watch?v=$1" 2>&1 > /dev/null || \
-yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" \
+yt-dlp $cookies --wait-for-video 1-15 --write-sub --sub-lang "live_chat" --sub-format "json" --live-from-start --skip-download -o "$output" "$1" \
 || echo "Error downloading chat for $1"
-        # If the stream is privated at the end (or some other event cuts your access to the stream),
-        # the chat file will not be closed correctly, and the .part extension can be removed
-        if [[ -f "$output.live_chat.json.part" ]]
-        then
-                mv "$output.live_chat.json.part" "$output.live_chat.json"
-        fi
-        # Compress the live_chat.json file, this can save 80-90% of the space usually taken by these files
-        zip -9 -m "$output.live_chat.zip" "$output.live_chat.json"  || echo "Error compressing chat for $1"
-} &
-chat_pid=$!
-# Download the metadata (.info.json) and thumbnail in parallel
-{
-        yt-dlp --cookies /app/cookies.txt --wait-for-video 1-15 -R 25 --live-from-start --write-info-json --write-thumbnail --convert-thumbnails png --write-description --skip-download -o "$output" "$1" \
-&& perl -pi -e "s/((?:[0-9]{1,3}\.){3}[0-9]{1,3})|((?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4})/0\.0\.0\.0/g" "$output.info.json" || echo "Error gathering video data (e.g. info.json) chat for $1"
-} &
-info_pid=$!
-mux_file=$(python /app/getMux.py)
-# Download the video/audio (from the start), preferring VP9 codec
-if [[ "$mux_file" == "False" ]]; then
-    ytarchive --cookies /app/cookies.txt -t --vp9 --retry-stream 15 --threads 4 --add-metadata --no-frag-files --write-mux-file --error --output "$output" "https://www.youtube.com/watch?v=$1" "best" \
-&& success="true" || (python /app/discord-web.py "$1" "error" ; kill -2 $chat_pid $info_pid)
-else
-    ytarchive --cookies /app/cookies.txt -t --vp9 --retry-stream 15 --threads 4 --add-metadata --no-frag-files --error --output "$output" "https://www.youtube.com/watch?v=$1" "best" \
-&& success="True" || (python /app/discord-web.py "$1" "error" ; kill -2 $chat_pid $info_pid)
+                # If the stream is privated at the end (or some other event cuts your access to the stream),
+                # the chat file will not be closed correctly, and the .part extension can be removed
+                if [[ -f "$output.live_chat.json.part" ]]
+                then
+                        mv "$output.live_chat.json.part" "$output.live_chat.json"
+                fi
+                # Compress the live_chat.json file, this can save 80-90% of the space usually taken by these files
+                zip -9 -m "$output.live_chat.zip" "$output.live_chat.json"  || echo "Error compressing chat for $1"
+        } &
+        chat_pid=$!
 fi
+infoOptions=$(python /app/getConfig.py "info_options")
+if ! [ -z "${infoOptions}"]; then
+        # Download the metadata (.info.json) and thumbnail in parallel
+        {
+                yt-dlp --wait-for-video 1-15 -R 25 --live-from-start $infoOptions --skip-download -o "$output" "$1" \
+&& (test -f "$output.info.json" && perl -pi -e "s/((?:[0-9]{1,3}\.){3}[0-9]{1,3})|((?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4})/0\.0\.0\.0/g" "$output.info.json") || echo "Error gathering video data (e.g. info.json) chat for $1"
+        } &
+        info_pid=$!
+fi
+
+#Download the video/audio (from the start), preferring VP9 codec
+ytarchiveOptions=$(python /app/getConfig.py "ytarchive_options")
+quality=$(python /app/getConfig.py "quality")
+ytarchive $ytarchiveOptions --error --output "$output" "https://www.youtube.com/watch?v=$1" "$quality" \
+&& success="True" || (python /app/discord-web.py "$1" "error" ; kill -2 $chat_pid $info_pid)
 
 # Wait for all above processes to complete
 wait
 
-
+mux_file=$(python /app/getConfig.py "mux_file")
 tempfolder=$(dirname "${tempdir}/${partialoutput}")
 if [[ "$success" == "True" ]]; then
+    echo "ytarchive for $1 was successful"
 #    tempfolder="${tempdir}/${partialoutput%/*}"
     outfolder=$(dirname "${donedir}/${partialoutput}")
     #Make parent folder
