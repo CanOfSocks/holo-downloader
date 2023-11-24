21f6b6e9c0ecf0a9ed9da7b8607042f07f4bd72c
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-24 16:09:59 +1100

    Delete cleanupZombies.sh

--- cleanupZombies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Get the process IDs (PIDs) of zombie processes
-zombie_pids=$(ps aux | awk '$8=="Z"{print $2}')
-sleep 5
-# Kill the zombie processes
-for pid in $zombie_pids; do
-    kill -9 $pid
-done
