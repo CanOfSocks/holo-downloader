ad66745ec3e7654c4ef09681e53aab07df538e6b
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-24 16:10:33 +1100

    Create downloadVid.sh

---
+++ downloadVid.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PROCESS_NAME=$(basename "$0")
+# Check if a process with the same script name is already running
+COUNT=$(pgrep -c -f "$PROCESS_NAME $1")
+#If more than one instance of script, it is already running so exit
+if [ "$COUNT" -gt 1 ]; then
+    echo "Downloader for $1 is already running, exiting...      Count=$COUNT"
+    return 0
+    exit
+fi
+/app/downloadVid.py "$1"
