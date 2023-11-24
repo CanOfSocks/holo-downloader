9f80db0ae3e49ccae9859ac4500753a1522a3aab
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-24 20:12:14 +1100

    Update downloadVid.sh

--- downloadVid.sh
+++ downloadVid.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 PROCESS_NAME=$(basename "$0")
 # Check if a process with the same script name is already running
-COUNT=$(pgrep -f "$PROCESS_NAME $1" | wc -l)
-
-# If more than one instance of the script, it is already running, so exit
-if [ "$COUNT" -gt 2 ]; then
-    #echo "Downloader for $1 is already running, exiting... Count=$COUNT"
-    exit 0
+COUNT=$(pgrep -c -f "$PROCESS_NAME $1")
+#If more than one instance of script, it is already running so exit
+if [ "$COUNT" -gt 1 ]; then
+    #echo "Downloader for $1 is already running, exiting...      Count=$COUNT"
+    return 0
+    exit
 fi
 python /app/downloadVid.py "$1"
 wait
