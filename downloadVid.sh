77dfd0f80a5122638a002ce73230cc73fa1e5ca1
CanOfSocks <100449497+CanOfSocks@users.noreply.github.com>
2023-11-24 16:39:51 +1100

    Modify to work with busybox

--- downloadVid.sh
+++ downloadVid.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
 PROCESS_NAME=$(basename "$0")
 # Check if a process with the same script name is already running
-COUNT=$(pgrep -c -f "$PROCESS_NAME $1")
-#If more than one instance of script, it is already running so exit
+COUNT=$(pgrep -f "$PROCESS_NAME $1" | wc -l)
+
+# If more than one instance of the script, it is already running, so exit
 if [ "$COUNT" -gt 1 ]; then
-    echo "Downloader for $1 is already running, exiting...      Count=$COUNT"
-    return 0
-    exit
+    echo "Downloader for $1 is already running, exiting... Count=$COUNT"
+    exit 0
 fi
 python /app/downloadVid.py "$1"
