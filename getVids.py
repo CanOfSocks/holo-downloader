57cf02506e4f64f9ba0f0e85f8f7cdb155c1b573
Alex Mckenzie-Hicks <github@canofsocks.com>
2024-06-01 16:15:19 +1000

    Fix unarchived execution for external/shell trigger

--- getVids.py
+++ getVids.py
@@ -2,16 +2,23 @@
 from getConfig import getFetchMethod
 from sys import argv
 command = None
+unarchived = False
 try:
     command = argv[1]
 except IndexError:
     command = None
+try:
+    if argv[2] == "unarchived":
+        unarchived = True
+except IndexError:
+    pass    
+
 method = getFetchMethod()
 if(method == "ytdlp"):
     import getYTDLP
-    getYTDLP.main(command)
+    getYTDLP.main(command,unarchived=unarchived)
 elif(method == "json"):
     import getJson
-    getJson.main(command)
+    getJson.main(command,unarchived=unarchived)
 else:
     print("Invalid method: {0}".format(method))
