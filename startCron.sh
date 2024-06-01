29525d0e0a2d63646174115169cbe7d4ff0525d8
Alex Mckenzie-Hicks <github@canofsocks.com>
2024-06-01 16:16:44 +1000

    Fix unarchived cron declarations

--- startCron.sh
+++ startCron.sh
@@ -30,9 +30,9 @@ END
     fi
 
     if [ -n "${UNARCHIVEDSCHEDULE}" ]; then
-        CRON_CONTENT+="${UNARCHIVEDSCHEDULE} /app/communityPosts.py > /proc/1/fd/1 2>/proc/1/fd/2"$'\n'
+        CRON_CONTENT+="${UNARCHIVEDSCHEDULE} /app/getVids.py 'unarchived' > /proc/1/fd/1 2>/proc/1/fd/2"$'\n'
     else
-        CRON_CONTENT+="*/30 * * * * /app/communityPosts.py > /proc/1/fd/1 2>/proc/1/fd/2"$'\n'
+        CRON_CONTENT+="*/30 * * * * /app/getVids.py 'unarchived' > /proc/1/fd/1 2>/proc/1/fd/2"$'\n'
     fi
 
     CRON_CONTENT+="@reboot python /app/discord_web.py '0' 'starting'"
