21c4c93b7d6b6d959f3407b43dcc0c74f4b2ec0d
Alex Mckenzie-Hicks <github@canofsocks.com>
2024-06-07 13:30:42 +1000

    Change sleep between stream initialisation to 2*number of streams to reduce likelihood of rate limiting

--- common.py
+++ common.py
@@ -18,7 +18,7 @@ def vid_executor(streams, command, unarchived = False):
             #Popen(command)
             Popen(command, start_new_session=True)
             from time import sleep
-            sleep(1.0)
+            sleep(2*len(streams))
     elif(command == "bash"):
         bash_array = ' '.join(streams)
         print(bash_array)
